42ae203e4bc45ceb30e3bf295b6e9c14d23c4f67
Andrew Renninger <awzrenn@gmail.com>
2020-03-18 18:38:12 -0400

    Update login.php

--- Owens Capstone Project/Capstone Project/login.php
+++ Owens Capstone Project/Capstone Project/login.php
@@ -15,7 +15,7 @@ $username = "admin";
 $password = "SixGuys1CapstoneProject";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=Capstone", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
