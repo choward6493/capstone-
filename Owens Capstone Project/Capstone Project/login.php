7ab63dd7aa21ad4657cc12b296716adfda87946c
Andrew Renninger <awzrenn@gmail.com>
2020-03-18 21:43:41 -0400

    Update login.php

--- Owens Capstone Project/Capstone Project/login.php
+++ Owens Capstone Project/Capstone Project/login.php
@@ -17,12 +17,36 @@ $password = "SixGuys1CapstoneProject";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
+/*
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+*/
+
+$usernamePP = $_POST["uname"];
+//$_POST["psw"];
+//$_POST["remember"];
+
+$hashPass = hash("md5",$_POST["psw"])
+$sql = "SELECT CustomerID, Email FROM Customers WHERE Email=$usernamePP";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "CustomerID: " . $row["CustomerID"]. " - Name: " . $row["Email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
+
+
+
 
 </body>
 </html>
