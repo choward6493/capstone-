c7de097376b96614562d34d17bce3e7f60848769
Andrew Renninger <awzrenn@gmail.com>
2020-03-19 02:00:20 -0400

    Update login.php

--- Owens Capstone Project/Capstone Project/login.php
+++ Owens Capstone Project/Capstone Project/login.php
@@ -17,7 +17,9 @@ $dbname="Capstone"
 
 // Create connection
 $conn = new mysqli($servername, $username, $password,$dbname);
-
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 /*
 
 // Check connection
