921b8ad3b229414ac6928f85c78e604b2d412839
Andrew Renninger <awzrenn@gmail.com>
2020-03-31 12:01:03 -0400

    d

--- Owens Capstone Project/Capstone Project/cartItem.php
+++ Owens Capstone Project/Capstone Project/cartItem.php
@@ -8,10 +8,10 @@ if(isset($_COOKIE['cart'])){
     $cart=json_decode($_COOKIE['cart']);
     $age = array("item"=>$item, "size"=>$dsize, "milk"=>$milk);
     array_push($cart,$age);
-    setcookie('cart',json_encode($cart), time() + (86400/24), "/");
+    setcookie('cart',json_encode($cart), time() + (86400), "/");
 }else{
     $age = array("item"=>$item, "size"=>$dsize, "milk"=>$milk);
-    setcookie('cart',json_encode(array($age)), time() + (86400/24), "/");
+    setcookie('cart',json_encode(array($age)), time() + (86400), "/");
 }
 
 /*
