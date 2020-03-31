ca6c1437aad471eb46d8c00a1c5b106eaa748797
Andrew Renninger <awzrenn@gmail.com>
2020-03-30 23:42:51 -0400

    Update cartItem.php

--- Owens Capstone Project/Capstone Project/cartItem.php
+++ Owens Capstone Project/Capstone Project/cartItem.php
@@ -4,11 +4,11 @@ $item=$_POST["item"];
 $dsize=$_POST["size"];
 $milk=$_POST["milk"];
 
-if(isset($_COOKIE['cart']){
-    //$cart=json_decode($_COOKIE['cart']);
-    //$age = array("item"=>$item, "size"=>$dsize, "milk"=>$milk);
-    //array_push($cart,$age);
-    //setcookie('cart',json_encode($cart), time() + (86400/24), "/");
+if(isset($_COOKIE['cart'])){
+    $cart=json_decode($_COOKIE['cart']);
+    $age = array("item"=>$item, "size"=>$dsize, "milk"=>$milk);
+    array_push($cart,$age);
+    setcookie('cart',json_encode($cart), time() + (86400/24), "/");
 }else{
     $age = array("item"=>$item, "size"=>$dsize, "milk"=>$milk);
     setcookie('cart',json_encode(array($age)), time() + (86400/24), "/");
