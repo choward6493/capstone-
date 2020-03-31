d9e2ac4561b6d285fe49a783412b789cb4b2a659
Andrew Renninger <awzrenn@gmail.com>
2020-03-30 22:08:23 -0400

    Update index.php

--- Owens Capstone Project/Capstone Project/index.php
+++ Owens Capstone Project/Capstone Project/index.php
@@ -103,7 +103,7 @@
             </script>
             <a href="cart.php"><img border="0" alt="Cart" src="pictures/cart.png" width="20" height="20" style="width:auto;float:right;font-family: Arial;"></a>
 
-            <button id="logB" display="none" class="login" onclick="document.getElementById('id01').style.display='block'" style="display:none;width:auto;float:right;font-family: Arial;">Login</button>
+            <button id="logB" display="inline" class="login" onclick="document.getElementById('id01').style.display='block'" style="display:none;width:auto;float:right;font-family: Arial;">Login</button>
             <button id="logOut" display="none" class="login" onclick="logMeOut();" style="display:none;width:auto;float:right;font-family: Arial;">Log Out</button>
             <button id="welcomeP" display="none" class="login" onclick="#" style="display:none;width:auto;float:right;font-family: Arial;">Welcome, <?php echo $customerName;?></button>
             
