1f0b6def509effef4a3fd677a6bdaea080012f02
asdfire <awzrenn@gmail.com>
2020-04-09 17:16:21 -0400

    Update completeOrder.php

--- Owens Capstone Project/Intranet/MontgomeryCup/public_html/completeOrder.php
+++ Owens Capstone Project/Intranet/MontgomeryCup/public_html/completeOrder.php
@@ -1,4 +1,22 @@
 <?php 
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+$servername = "capstone2.cxiblbeokqky.us-east-1.rds.amazonaws.com:1433";
+$username = "admin";
+$password = "SixGuys1CapstoneProject";
+$dbname="Capstone";
+$loggedIn=false;
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 $location=$_POST['locationVal'];
 $orderID=$_POST['orderID'];
 
