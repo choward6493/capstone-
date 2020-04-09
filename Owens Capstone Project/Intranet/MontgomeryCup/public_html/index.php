8a574200bf64b145c89f656ef6e225372429f57d
asdfire <awzrenn@gmail.com>
2020-04-09 12:02:51 -0400

    Update index.php

--- Owens Capstone Project/Intranet/MontgomeryCup/public_html/index.php
+++ Owens Capstone Project/Intranet/MontgomeryCup/public_html/index.php
@@ -220,13 +220,11 @@ with a smooth aroma, Bagels, Muffins and Organic Snacks.">
             while($row2 = $result->fetch_array(MYSQLI_NUM)){
               array_push($itemsArray,$row2);
             }
-            $console_log($itemsArray);
             foreach($itemsArray as $itemSingle){
               //ask products where productID
 
               $sql4= 'SELECT ProductName FROM Products WHERE ProductID='.$itemSingle["ProductID"].'';
               $result4= $conn->query($sql4);
-              console_log($result4);
               $productName= $result->fetch_assoc()["ProductName"];
               $itemsString=$productName.' - '.$itemSingle["OrderSize"].' x '.strval($itemSingle["ItemQuantity"]);
               echo $itemsString.'<br/>';
