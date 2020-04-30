bd760748706fb31e45d97932ebaae652c2e5d041
Andrew Renninger <awzrenn@gmail.com>
2020-04-30 13:51:28 -0400

    Update menuItem.php

--- Owens Capstone Project/Capstone Project/menuItem.php
+++ Owens Capstone Project/Capstone Project/menuItem.php
@@ -220,6 +220,7 @@ window.onclick = function(event) {
   <div class="description" style="margin-left:850px; padding-top:100px;font-size:20px;">
   <h2><?php 
   $sql='SELECT DescriptionInfo FROM Products WHERE ProductName="'.$item.'"';
+  console_log($sql);
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
       $descInfo=$result->fetch_assoc()["DescriptionInfo"];
