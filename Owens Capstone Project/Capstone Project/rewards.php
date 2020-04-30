22197dc25fea7f6771eb2ea4bf5bccff75c74acd
Andrew Renninger <awzrenn@gmail.com>
2020-04-30 11:13:33 -0400

    a

--- Owens Capstone Project/Capstone Project/rewards.php
+++ Owens Capstone Project/Capstone Project/rewards.php
@@ -37,8 +37,25 @@
     while($row=$result->fetch_array(MYSQLI_ASSOC)){
         $orderd[]=$row;
     }
-    console_log($orderd);
-
+    //console_log($orderd);
+    foreach($products as $product){
+        $numBought[]=0;
+    }
+    foreach($orderd as $orderS){
+        $numBought[((int)$orderS["ProductID"]-1)]+=1;
+    }
+    
+    function getMostPopular(){
+        //get biggest amount of bought, returns array item with most
+        $big=array(0,0);
+        for($i=0;$i<count($numBought);$i++;){
+            if($numBought[$i]>=$big[1]){
+                $big[0]=$i;
+                $big[1]=$numBought[$i];
+            }
+        }
+        return $big[0];
+    }
     //if login data is stored, check that it is actual
     if(isset($_COOKIE['token'])&&isset($_COOKIE['user'])){
         $usernamePP=$_COOKIE['user'];
@@ -169,7 +186,12 @@ window.onclick = function(event) {
 </script>
 </div>
 
-
+<div class="card">
+<h2>Stats</h2>
+<h5>Most Popular</h5>
+<p>The most popular item is the <?php echo $products[getMostPopular()]["ProductName"];?>, which costs $<?php echo $products[getMostPopular()]["Cost"];?>, and is <?php if($products[getMostPopular()]["Availible"]!="1"{echo "not";}?> availible</p>
+ </div>
+        
 
 
 <div class="footer">
