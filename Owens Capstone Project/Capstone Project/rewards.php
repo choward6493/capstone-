6c5b1846e2626b957ed456a9686e18a725248751
Andrew Renninger <awzrenn@gmail.com>
2020-04-30 10:34:07 -0400

    update

--- Owens Capstone Project/Capstone Project/rewards.php
+++ Owens Capstone Project/Capstone Project/rewards.php
@@ -28,9 +28,16 @@
     */
     //get buy stats
     $sql='SELECT * FROM Products';
+    $result = $conn->query($sql);
+    while($row=$result->fetch_array(MYSQLI_ASSOC)){
+        $products[]=$row;
+    }
     $sql='SELECT * FROM OrderDetails';
     $result = $conn->query($sql);
-    
+    while($row=$result->fetch_array(MYSQLI_ASSOC)){
+        $orderd[]=$row;
+    }
+    console.log($orderd);
 
     //if login data is stored, check that it is actual
     if(isset($_COOKIE['token'])&&isset($_COOKIE['user'])){
