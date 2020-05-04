f43a7980dbb14d2c3f2aa10c8eddbf4fd28b83a5
Andrew Renninger <awzrenn@gmail.com>
2020-05-04 13:38:51 -0400

    Update order.php

--- Owens Capstone Project/Intranet/MontgomeryCup/public_html/order.php
+++ Owens Capstone Project/Intranet/MontgomeryCup/public_html/order.php
@@ -8,6 +8,75 @@
 <link rel="stylesheet" href="index.css">
 <script src="processing.min.js"></script>
 <?php 
+function console_log($output, $with_script_tags = true) {
+  $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+  if ($with_script_tags) {
+      $js_code = '<script>' . $js_code . '</script>';
+  }
+  echo $js_code;
+}
+$servername = "capstone2.cxiblbeokqky.us-east-1.rds.amazonaws.com:1433";
+$username = "admin";
+$password = "SixGuys1CapstoneProject";
+$dbname="Capstone";
+
+
+//INSERT INTO Employees(FirstName,LastName,PhoneNumber,Email,Status,HireDate,Title,AccessCode)Values("Test","Dummy","614-999-9999","arenninger@student.cscc.edu","Hired","2020-01-01","Manager","123456")
+//INSERT INTO EmployeeLOG(EmployeeID,PasswordHash)values(1,"343b1c4a3ea721b2d640fc8700db0f36")
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_COOKIE['token'])&&isset($_COOKIE['user'])){
+  $usernamePP=$_COOKIE['user'];
+  //console_log($usernamePP);
+  $hashPass=$_COOKIE['token'];
+  $userID=0;
+  $sql = 'SELECT EmployeeID FROM Employees WHERE Email="'.$usernamePP.'"';
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output userID from email
+      $userID=$result->fetch_assoc()["EmployeeID"];
+      //echo $result->fetch_assoc()["CustomerID"].'<br>';
+      $sql2 = 'SELECT PasswordHash FROM EmployeeLOG WHERE EmployeeID='.$userID;
+      //echo $sql2;
+      $result2 = $conn->query($sql2);
+      if ($result2->num_rows > 0) {
+          // output userID from email
+          $hashedData=$result2->fetch_assoc()["PasswordHash"];
+          //echo '<br>'.$hashedData.'<br>';
+          //echo '<br>'.$hashPass;
+          if($hashPass==$hashedData){
+            //get employee name
+              $sql = 'SELECT * FROM Employees WHERE EmployeeID='.$userID;
+              $result = $conn->query($sql);
+              $employeeName=$result->fetch_assoc()["FirstName"];
+              //get employee title
+              $sql = 'SELECT * FROM Employees WHERE EmployeeID='.$userID;
+              $result = $conn->query($sql);
+              $employeeTitle=$result->fetch_assoc()["Title"];
+              $loggedIn=true;
+              //echo 'test';
+
+          }else {
+              //echo "Password not right";
+          }
+      } else {
+        //echo "nothing found";
+      }
+  } else {
+      //echo "0 results";
+
+  }
+}else{
+  $loggedIn=false;
+}
 $location=array_values($_GET);
 console_log($location);
 ?>
