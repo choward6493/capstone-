3ccaac594e9b16a024d87aef492213f93606c1d6
Andrew Renninger <awzrenn@gmail.com>
2020-05-07 16:15:30 -0400

    Update cart.php

--- Owens Capstone Project/Capstone Project/cart.php
+++ Owens Capstone Project/Capstone Project/cart.php
@@ -341,7 +341,7 @@ window.onclick = function(event) {
 
         </div>
         <label for="picktime">Pickup Time</label>
-        <input type="datetime-local" id="picktime" name="picktime" min="<?php echo date('m-d-Y\TH:i:s.v'); ?>">
+        <input type="datetime-local" id="picktime" name="picktime" min="<?php echo date('y-m-d\TH:i:s'); ?>">
         <label for="location">Pickup Location</label>
             <select id='location' name="location" required>
                             <option value="GC">Grove City</option>
