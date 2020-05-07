669e7ee7bfb2916ae35c79f5da7544442de2be34
Andrew Renninger <awzrenn@gmail.com>
2020-05-07 12:58:52 -0400

    Create index.php

---
+++ Owens Capstone Project/Capstone Project/files/index.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+    <iframe src="https://drive.google.com/embeddedfolderview?id=1vKC3ypEznT_RS231jn2-4zCDTxkFfmd_#list" style="width:100%; height:600px; border:0;"></iframe>
+        
+    </body>
+</html>
